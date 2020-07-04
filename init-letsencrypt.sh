7a2eb3e183bc035ab3eaaed6e015a8b76f3756a7
Edenn <deozza@gmail.com>
2020-07-05 01:47:11 +0200

    try ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(apigateway.appquarium.fr apispecies.appquarium.fr apiuser.appquarium.fr)
+domains=(*.appquarium.fr)
 rsa_key_size=4096
 data_path="./.docker/certbot"
 email="deozza@gmail.com" # Adding a valid address is strongly recommended
